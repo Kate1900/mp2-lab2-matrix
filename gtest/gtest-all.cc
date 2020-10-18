71e589df484442bcc58179bce010deeb1b5b4bcb
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2020-10-18 21:55:45 +0300

    lab 2

--- gtest/gtest-all.cc
+++ gtest/gtest-all.cc
@@ -109,7 +109,6 @@
 #ifndef GTEST_INCLUDE_GTEST_GTEST_SPI_H_
 #define GTEST_INCLUDE_GTEST_GTEST_SPI_H_
 
-
 namespace testing {
 
 // This helper class can be used to mock out Google Test failure reporting
