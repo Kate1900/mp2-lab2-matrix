e06b12ccdecc0d282948e9938ed0d059bf91b683
Kate1900 <61184952+Kate1900@users.noreply.github.com>
2020-10-18 22:00:51 +0300

    lab 2

---
+++ vector/main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include "MyMatrix.h"
+
+
+int main()
+{
+	TMatrix<int> a(5), b(5), c(5);
+	int i, j;
+
+	for (i = 0; i < 5; i++)
+		for (j = i; j < (i + 1); j++)
+		{
+			a[i][j] = i * 10 + j;
+			b[i][j] = (i * 10 + j) * 100;
+		}
+	c = b + a;
+	cout << "Matrix a = " << endl << a << endl;
+	cout << "Matrix b = " << endl << b << endl;
+	cout << "Matrix c = a + b" << endl << c << endl;
+
+	return 0;
+}
+
